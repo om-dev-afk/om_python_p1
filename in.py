65daaf0790584b3f0da29f572298db6e8db00efa
Your Name <weddingsatyam@gmail.com>
2026-08-22 06:50:15 +0530

    one

---
+++ in.py
@@ -0,0 +1,12 @@
+# a = int(input("what is your age :"))
+
+# print(f"your are {a} years old.")
+a = 33
+b = 11
+
+c = a/b
+d = a//b
+print (c)
+print (d)
+print (5**3)
+print (13%2)
---
+++ test.py
@@ -0,0 +1,25 @@
+'''
+so here we are starting ,atleast somewhere.
+so how should we start.
+point by point
+from geeks from geeks
+or w3 schools
+or campus x ,etc
+
+so i have to prepare for python, sql, numpy, pandas, machine learning, also goggle cloud, and side by side 
+spoken english.
+
+15/08/2026
+
+content rewards
+/explodedview
+/xray
+/handwritting
+
+
+
+remember to sign up
+user testing
+leeway
+maze
+'''
